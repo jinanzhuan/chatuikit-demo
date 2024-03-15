a627341ad374b16b35d0edece0f933bde3d91cb0
apex-wang <1746807718@qq.com>
2024-03-15 16:49:11 +0800

    MainActivity add request count

--- app/src/main/kotlin/com/hyphenate/chat/demo/viewmodel/ChatClientRepository.kt
+++ app/src/main/kotlin/com/hyphenate/chat/demo/viewmodel/ChatClientRepository.kt
@@ -1,6 +1,7 @@
 package com.hyphenate.chat.demo.viewmodel
 
 import com.hyphenate.easeui.common.ChatClient
+import com.hyphenate.easeui.feature.invitation.helper.EaseNotificationMsgManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -16,6 +17,15 @@ class ChatManagerRepository: BaseRepository() {
     suspend fun getAllUnreadMessageCount(): Int =
         withContext(Dispatchers.IO) {
             ChatClient.getInstance().chatManager().unreadMessageCount
+        }
+
+    /**
+     * Get all unread request count.
+     */
+    suspend fun getRequestUnreadCount():Int =
+        withContext(Dispatchers.IO) {
+            val systemConversation = EaseNotificationMsgManager.getInstance().getConversation()
+            systemConversation.unreadMsgCount
         }
 
 }
