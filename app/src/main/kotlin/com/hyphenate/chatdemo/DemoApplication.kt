43783d840578abb038ed808861297c2efd76eb59
jinan <neil.liu@easemob.com>
2024-03-20 16:00:34 +0800

    Remove unnecessary code in DemoApplication.

--- app/src/main/kotlin/com/hyphenate/chatdemo/DemoApplication.kt
+++ app/src/main/kotlin/com/hyphenate/chatdemo/DemoApplication.kt
@@ -3,10 +3,6 @@ package com.hyphenate.chatdemo
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import com.hyphenate.chatdemo.base.UserActivityLifecycleCallbacks
-import com.hyphenate.chatdemo.login.LoginActivity
-import com.hyphenate.easeui.EaseIM
-import com.hyphenate.easeui.common.ChatConnectionListener
-import com.hyphenate.easeui.common.ChatLog
 import com.hyphenate.easeui.common.helper.EasePreferenceManager
 import com.scwang.smart.refresh.footer.ClassicsFooter
 import com.scwang.smart.refresh.header.ClassicsHeader
@@ -21,27 +17,6 @@ class DemoApplication: Application() {
 
         DemoHelper.getInstance().init(this)
         initSDK()
-
-
-        EaseIM.addConnectionListener(object : ChatConnectionListener {
-            override fun onConnected() {
-
-            }
-
-            override fun onDisconnected(errorCode: Int) {
-                ChatLog.e("app","onDisconnected: $errorCode")
-            }
-
-            override fun onLogout(errorCode: Int, info: String?) {
-                super.onLogout(errorCode, info)
-                ChatLog.e("app","onLogout: $errorCode")
-                mLifecycleCallbacks.activityList.forEach {
-                    it.finish()
-                }
-                LoginActivity.startAction(instance)
-            }
-
-        })
 
         // Call this method after EaseIM#init
         val isBlack = EasePreferenceManager.getInstance().getBoolean("isBlack")
