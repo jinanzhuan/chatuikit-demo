29ee04a4f9d20511958c270f7370a407c1fd780d
apex-wang <1746807718@qq.com>
2024-03-20 21:28:01 +0800

    update detail menu

--- app/src/main/kotlin/com/hyphenate/chatdemo/ui/contact/ChatContactDetailActivity.kt
+++ app/src/main/kotlin/com/hyphenate/chatdemo/ui/contact/ChatContactDetailActivity.kt
@@ -1,25 +1,48 @@
 package com.hyphenate.chatdemo.ui.contact
 
+import android.app.Activity
+import android.content.Intent
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.hyphenate.chatdemo.R
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import com.hyphenate.chatdemo.callkit.CallKitManager
+import com.hyphenate.chatdemo.viewmodel.ProfileInfoViewModel
 import com.hyphenate.easeui.feature.contact.EaseContactDetailsActivity
 import com.hyphenate.easeui.model.EaseMenuItem
 import com.hyphenate.easeui.widget.EaseArrowItemView
 
 
 class ChatContactDetailActivity:EaseContactDetailsActivity() {
-
+    private lateinit var model: ProfileInfoViewModel
     private val remarkItem: EaseArrowItemView by lazy { findViewById(R.id.item_remark) }
+
+    companion object {
+        private const val REQUEST_UPDATE_REMARK = 100
+        private const val RESULT_UPDATE_REMARK = "result_update_remark"
+    }
+
+    private val launcherToUpdateRemark: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result -> onActivityResult(result, REQUEST_UPDATE_REMARK) }
 
     override fun initView() {
         super.initView()
+        model = ViewModelProvider(this)[ProfileInfoViewModel::class.java]
+        user?.let {
+            val remark = model.fetchLocalUserRemark(it.userId)
+            remarkItem.setContent(remark)
+        }
     }
 
     override fun initListener() {
         super.initListener()
         remarkItem.setOnClickListener{
-
+            user?.let {
+                launcherToUpdateRemark.launch(ChatContactRemarkActivity.createIntent(mContext,it.userId))
+            }
         }
     }
 
@@ -62,5 +85,21 @@ class ChatContactDetailActivity:EaseContactDetailsActivity() {
             }
         }
         return false
+    }
+
+    private fun onActivityResult(result: ActivityResult, requestCode: Int) {
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data = result.data
+            when (requestCode) {
+                REQUEST_UPDATE_REMARK ->{
+                    data?.let {
+                        if (it.hasExtra(RESULT_UPDATE_REMARK)){
+                            remarkItem.setContent(it.getStringExtra(RESULT_UPDATE_REMARK))
+                        }
+                    }
+                }
+                else -> {}
+            }
+        }
     }
 }
